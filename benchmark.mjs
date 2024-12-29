f8a08a8d48b28c04eb847e82505cd0a03b760cd7
gentlee <d.gentlee@gmail.com>
2024-12-29 16:49:30 +0300

    refactor running benchmarks and gc after each properly

--- benchmark.mjs
+++ benchmark.mjs
@@ -26,145 +26,149 @@ const actions = {
   }),
 }
 
-const results = {}
-
-const vanillaReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'test/addItem':
-      return {
-        ...state,
-        array: [...state.array, action.payload],
+const reducers = {
+  'no-immer': (state = initialState, action) => {
+    switch (action.type) {
+      case 'test/addItem':
+        return {
+          ...state,
+          array: [...state.array, action.payload],
+        }
+      case 'test/removeItem':
+        return {
+          ...state,
+          array: state.array.filter((_, i) => i !== action.payload),
+        }
+      case 'test/updateItem':
+        return {
+          ...state,
+          array: state.array.map((item) =>
+            item.id === action.payload.id
+              ? {
+                  ...item,
+                  value: action.payload.value,
+                  nested: {...item.nested, data: action.payload.nestedData},
+                }
+              : item
+          ),
+        }
+      case 'test/concatArray': {
+        const newArray = action.payload.concat(state.array)
+        newArray.length = initialState.array.length
+        return {
+          ...state,
+          array: newArray,
+        }
       }
-    case 'test/removeItem':
-      return {
-        ...state,
-        array: state.array.filter((_, i) => i !== action.payload),
-      }
-    case 'test/updateItem':
-      return {
-        ...state,
-        array: state.array.map((item) =>
-          item.id === action.payload.id
-            ? {
-                ...item,
-                value: action.payload.value,
-                nested: {...item.nested, data: action.payload.nestedData},
-              }
-            : item
-        ),
-      }
-    case 'test/concatArray': {
-      const newArray = action.payload.concat(state.array)
-      newArray.length = initialState.array.length
-      return {
-        ...state,
-        array: newArray,
-      }
+      default:
+        return state
     }
-    default:
-      return state
-  }
+  },
+  immer: createSlice({
+    name: 'test',
+    initialState,
+    reducers: {
+      addItem: (state, action) => {
+        state.array.push(action.payload)
+      },
+      removeItem: (state, action) => {
+        state.array.splice(action.payload, 1)
+      },
+      updateItem: (state, action) => {
+        const item = state.array.find((item) => item.id === action.payload.id)
+        item.value = action.payload.value
+        item.nested.data = action.payload.nestedData
+      },
+      concatArray: (state, action) => {
+        state.array.unshift(...action.payload)
+        state.array.length = initialState.array.length
+      },
+    },
+  }).reducer,
+  'immer-immutable': createSlice({
+    name: 'test',
+    initialState,
+    reducers: {
+      addItem: (state, action) => {
+        return {
+          ...state,
+          array: [...original(state).array, action.payload],
+        }
+      },
+      removeItem: (state, action) => {
+        return {
+          ...state,
+          array: original(state).array.filter((_, i) => i !== action.payload),
+        }
+      },
+      updateItem: (state, action) => {
+        return {
+          ...state,
+          array: original(state).array.map((item) =>
+            item.id === action.payload.id
+              ? {
+                  ...item,
+                  value: action.payload.value,
+                  nested: {...item.nested, data: action.payload.nestedData},
+                }
+              : item
+          ),
+        }
+      },
+      concatArray: (state, action) => {
+        const newArray = action.payload.concat(original(state).array)
+        newArray.length = initialState.array.length
+        return {
+          ...state,
+          array: newArray,
+        }
+      },
+    },
+  }).reducer,
 }
 
-const {reducer: immerReducer} = createSlice({
-  name: 'test',
-  initialState,
-  reducers: {
-    addItem: (state, action) => {
-      state.array.push(action.payload)
-    },
-    removeItem: (state, action) => {
-      state.array.splice(action.payload, 1)
-    },
-    updateItem: (state, action) => {
-      const item = state.array.find((item) => item.id === action.payload.id)
-      item.value = action.payload.value
-      item.nested.data = action.payload.nestedData
-    },
-    concatArray: (state, action) => {
-      state.array.unshift(...action.payload)
-      state.array.length = initialState.array.length
-    },
-  },
-})
-
-const {reducer: immerReducerImmutable} = createSlice({
-  name: 'test',
-  initialState,
-  reducers: {
-    addItem: (state, action) => {
-      return {
-        ...state,
-        array: [...original(state).array, action.payload],
-      }
-    },
-    removeItem: (state, action) => {
-      return {
-        ...state,
-        array: original(state).array.filter((_, i) => i !== action.payload),
-      }
-    },
-    updateItem: (state, action) => {
-      return {
-        ...state,
-        array: original(state).array.map((item) =>
-          item.id === action.payload.id
-            ? {
-                ...item,
-                value: action.payload.value,
-                nested: {...item.nested, data: action.payload.nestedData},
-              }
-            : item
-        ),
-      }
-    },
-    concatArray: (state, action) => {
-      const newArray = action.payload.concat(original(state).array)
-      newArray.length = initialState.array.length
-      return {
-        ...state,
-        array: newArray,
-      }
-    },
-  },
-})
+const results = {}
 
 /**
- * @param {keyof typeof actions} actionKey
- * @param {'no-immer' | 'immer' | 'immer no-auto-freeze' | 'immer immutable' | 'immer immutable no-auto-freeze'} variant
- * @param {() => import('redux').Store} setupFn
+ * @param {string} actionKey
+ * @param {keyof typeof reducers} reducerKey
+ * @param {boolean} autoFreeze
  */
-function benchmark(actionKey, variant, setupFn) {
-  // Setup
+const benchmark = (actionKey, reducerKey, autoFreeze = true) => {
+  {
+    // Setup
 
-  const warmupCount = 100
-  const runCount = 1000
-  const store = setupFn()
-  const action = actions[actionKey]
-  const run = (store, index) => store.dispatch(action(index))
+    setAutoFreeze(autoFreeze)
+    const warmupCount = 100
+    const runCount = 1000
+    const action = actions[actionKey]
+    const store = configureStore({reducer: reducers[reducerKey]})
+    const run = (store, index) => store.dispatch(action(index))
 
-  // Warmup
+    // Warmup
 
-  for (let i = 0; i < warmupCount; i += 1) {
-    run(store, i)
+    for (let i = 0; i < warmupCount; i += 1) {
+      run(store, i)
+    }
+
+    globalThis.gc()
+
+    // Measurement
+
+    const start = performance.now()
+    for (let i = 0; i < runCount; i += 1) {
+      run(store, i)
+    }
+    const end = performance.now()
+
+    // Log results
+
+    const variant = `${reducerKey}${autoFreeze ? '' : ' no-auto-freeze'}`
+    const result = (end - start) / runCount
+    results[actionKey] ??= {}
+    results[actionKey][variant] = result
+    console.log(`${actionKey} [${variant}]: ${result.toFixed(4)} ms`)
   }
-
-  globalThis.gc()
-
-  // Measurement
-
-  const start = performance.now()
-  for (let i = 0; i < runCount; i += 1) {
-    run(store, i)
-  }
-  const end = performance.now()
-
-  // Log results
-
-  const result = (end - start) / runCount
-  results[actionKey] ??= {}
-  results[actionKey][variant] = result
-  console.log(`${actionKey} [${variant}]: ${result.toFixed(4)} ms`)
 
   globalThis.gc()
 }
@@ -179,101 +183,13 @@ console.log(`      _             _   _               _                     _    
                                 |___/                                                             
 `)
 
-benchmark('add', 'no-immer', () => {
-  return configureStore({reducer: vanillaReducer})
-})
-
-benchmark('add', 'immer', () => {
-  setAutoFreeze(true)
-  return configureStore({reducer: immerReducer})
-})
-
-benchmark('add', 'immer no-auto-freeze', () => {
-  setAutoFreeze(false)
-  return configureStore({reducer: immerReducer})
-})
-
-benchmark('add', 'immer immutable', () => {
-  setAutoFreeze(true)
-  return configureStore({reducer: immerReducerImmutable})
-})
-
-benchmark('add', 'immer immutable no-auto-freeze', () => {
-  setAutoFreeze(false)
-  return configureStore({reducer: immerReducerImmutable})
-})
-
-benchmark('remove', 'no-immer', () => {
-  return configureStore({reducer: vanillaReducer})
-})
-
-benchmark('remove', 'immer', () => {
-  setAutoFreeze(true)
-  return configureStore({reducer: immerReducer})
-})
-
-benchmark('remove', 'immer no-auto-freeze', () => {
-  setAutoFreeze(false)
-  return configureStore({reducer: immerReducer})
-})
-
-benchmark('remove', 'immer immutable', () => {
-  setAutoFreeze(true)
-  return configureStore({reducer: immerReducerImmutable})
-})
-
-benchmark('remove', 'immer immutable no-auto-freeze', () => {
-  setAutoFreeze(false)
-  return configureStore({reducer: immerReducerImmutable})
-})
-
-benchmark('update', 'no-immer', () => {
-  return configureStore({reducer: vanillaReducer})
-})
-
-benchmark('update', 'immer', () => {
-  setAutoFreeze(true)
-  return configureStore({reducer: immerReducer})
-})
-
-benchmark('update', 'immer no-auto-freeze', () => {
-  setAutoFreeze(false)
-  return configureStore({reducer: immerReducer})
-})
-
-benchmark('update', 'immer immutable', () => {
-  setAutoFreeze(true)
-  return configureStore({reducer: immerReducerImmutable})
-})
-
-benchmark('update', 'immer immutable no-auto-freeze', () => {
-  setAutoFreeze(false)
-  return configureStore({reducer: immerReducerImmutable})
-})
-
-benchmark('concat', 'no-immer', () => {
-  return configureStore({reducer: vanillaReducer})
-})
-
-benchmark('concat', 'immer', () => {
-  setAutoFreeze(true)
-  return configureStore({reducer: immerReducer})
-})
-
-benchmark('concat', 'immer no-auto-freeze', () => {
-  setAutoFreeze(false)
-  return configureStore({reducer: immerReducer})
-})
-
-benchmark('concat', 'immer immutable', () => {
-  setAutoFreeze(true)
-  return configureStore({reducer: immerReducerImmutable})
-})
-
-benchmark('concat', 'immer immutable no-auto-freeze', () => {
-  setAutoFreeze(false)
-  return configureStore({reducer: immerReducerImmutable})
-})
+for (let actionKey in actions) {
+  benchmark(actionKey, 'no-immer')
+  benchmark(actionKey, 'immer')
+  benchmark(actionKey, 'immer', false)
+  benchmark(actionKey, 'immer-immutable')
+  benchmark(actionKey, 'immer-immutable', false)
+}
 
 console.table(results)
 
